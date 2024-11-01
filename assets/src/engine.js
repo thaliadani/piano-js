413b7704d6d8262cddf02c8a8d02d168d000bb28
thaliadani <thaliadani2@gmail.com>
2024-11-01 16:45:30 -0700

    modified:   assets/src/engine.js 	modified:   assets/styles/main.css

--- assets/src/engine.js
+++ assets/src/engine.js
@@ -2,6 +2,8 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 const volumeSlider = document.querySelector(".volume-slider input");
 
+const keyCheck = document.querySelector(".key-check input");
+
 let mapedKeys = [];
 let audio = new Audio("./assets/tunes/a.wav");
 
@@ -33,4 +35,10 @@ const handleVolume = (e) => {
     audio.volume = e.target.value;
 };
 
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
 volumeSlider.addEventListener("input", handleVolume);
+
+keyCheck.addEventListener("click", showHideKeys);
